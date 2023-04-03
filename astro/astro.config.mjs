bb3fe13d16c3c9955b279690e3825c54a748291e
Antoine Latrille <antoine.latrille@gmail.com>
2023-04-03 12:30:17 +0200

    Checkpoint before dropping evrything:

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,8 +1,58 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import unocss from '@unocss/astro';
+import presetIcons from '@unocss/preset-icons';
+import logos from '@iconify-json/logos/icons.json';
+import uil from '@iconify-json/uil/icons.json';
+import presetWind from '@unocss/preset-wind';
+import presetTypography from '@unocss/preset-typography';
 
-import solidJs from "@astrojs/solid-js";
+const colors = [
+  'red',
+  'blue',
+  'green',
+  'yellow',
+  'pink',
+  'purple',
+  'orange',
+  'teal',
+  'cyan',
+  'lime',
+  'emerald',
+  'fuchsia',
+  'violet',
+  'rose',
+  'sky',
+  'amber',
+];
 
-// https://astro.build/config
+const getSafeList = () => {
+  const list = [];
+  colors.forEach((color) => {
+    list.push(`hover:bg-${color}`);
+    list.push(`bg-${color}`);
+    list.push(`prose-${color}`);
+  });
+  return list;
+};
+
 export default defineConfig({
-  integrations: [solidJs()]
+  site: 'https://brutal.elian.codes/',
+  trailingSlash: 'ignore',
+  integrations: [
+    sitemap(),
+    unocss({
+      presets: [
+        presetWind(),
+        presetIcons({
+          collections: {
+            logos,
+            uil,
+          },
+        }),
+        presetTypography(),
+      ],
+      safelist: getSafeList(),
+    }),
+  ],
 });
