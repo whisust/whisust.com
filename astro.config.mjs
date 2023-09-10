d0f6dc60a16480c4f1b5407aa098f02f328f867d
Antoine Latrille <antoine.latrille@gmail.com>
2023-09-10 22:53:52 +0200

    feat(astro): bump astro to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,6 @@ import UnoCSS from 'unocss/astro';
 export default defineConfig({
   site: 'https://www.whisust.com/',
   trailingSlash: 'ignore',
-  experimental: {
-    assets: true
-  },
   integrations: [
     sitemap(),
     UnoCSS({injectReset: true}),
