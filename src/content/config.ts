ee0e51d58a98d2bb24b45edc5c451e413893ea16
Antoine Latrille <antoine.latrille@gmail.com>
2023-09-10 22:55:05 +0200

    feat(transitions): use view transitions for blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const blogCollection = defineCollection({
 		author: z.string(),
 		tags: z.array(z.string()),
 		description: z.string(),
-		pubDate: z.string().transform((str) => new Date(str)),
+		pubDate: z.string().transform((str: any) => new Date(str)),
 		imgUrl: z.string(),
 		draft: z.boolean().optional().default(false),
 	}),
